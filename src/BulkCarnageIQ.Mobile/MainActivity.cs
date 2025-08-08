f88b3ef6538ec7f84f9cac30099cdb645b808f34
dmckay <dmckay@managewaresolutions.com>
2025-08-08 13:27:06 -0700

    DM - Not MAUI but Native Android, good enough for starting off :D

---
+++ src/BulkCarnageIQ.Mobile/MainActivity.cs
@@ -0,0 +1,16 @@
+using Android.App;
+using Android.OS;
+
+namespace BulkCarnageIQ.Mobile;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+    }
+}
